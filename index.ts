97b7104722298ec95be9b6b29bb155bb5830b1d9
BenReed161 <benreed447@gmail.com>
2024-05-28 18:06:28 -0700

    initial commit

---
+++ index.ts
@@ -0,0 +1,6 @@
+import * as aws from "@pulumi/aws";
+import * as command from "@pulumi/command";
+import * as pulumi from "@pulumi/pulumi";
+import * as fs from "fs";
+
+const config = new pulumi.Config();
